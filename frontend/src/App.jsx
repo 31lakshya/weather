232da01c36204ef545e936a1e1eb3db954888fc3
31lakshya <lakshya.neel31@gmail.com>
2026-09-07 00:25:10 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 const API_URL = import.meta.env.PROD
-    ? "/api"
+    ? "https://weather-q81x.onrender.com/api"
     : "http://localhost:5000/api";
 
 function App() {
